a13398017a11fb95a77bdfeaa46839a3b690b068
ThaoHoNhu <nhuthao911@gmail.com>
2022-10-22 10:25:28 +0700

    lesson-26, change sourcecode

--- src/main/java/tests/authen/LoginTestWithBaseTest.java
+++ src/main/java/tests/authen/LoginTestWithBaseTest.java
@@ -1,6 +1,8 @@
-package tests.parallel;
+package tests.authen;
 
-import io.qameta.allure.Description;
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.qameta.allure.TmsLink;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -9,23 +11,21 @@ import test_data.models.LoginCred;
 import test_flows.authentication.LoginFlow;
 import tests.BaseTest;
 
-public class Login extends BaseTest {
+public class LoginTestWithBaseTest extends BaseTest {
 
-    @Description("Login Test with data driven")
-    @Test(dataProvider = "loginCredData", description = "Login Test")
+
     @TmsLink("TEST-123")
+    @Test(dataProvider = "loginCredData")
     public void testLogin(LoginCred loginCred) {
-        System.out.println("--> Session ID: " + getDriver().getSessionId());
         LoginFlow loginFlow = new LoginFlow(getDriver(), loginCred.getEmail(), loginCred.getPassword());
         loginFlow.gotoLoginScreen();
         loginFlow.login();
         loginFlow.verifyLogin();
     }
 
-
     @DataProvider
     public LoginCred[] loginCredData() {
-        String filePath = "/src/test/java/test_data/authen/LoginCreds.json";
+        String filePath = "/src/main/java/test_data/authen/LoginCreds.json";
         return DataObjectBuilder.buildDataObject(filePath, LoginCred[].class);
     }
 
--- src/main/java/tests/authen/LoginTestWithDataBuilder.java
+++ src/main/java/tests/authen/LoginTestWithDataBuilder.java
@@ -3,29 +3,34 @@ package tests.authen;
 import driver.DriverFactory;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.qameta.allure.TmsLink;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import platform.Platform;
 import test_data.DataObjectBuilder;
 import test_data.models.LoginCred;
 import test_flows.authentication.LoginFlow;
-import tests.BaseTest;
 
-public class LoginTestWithBaseTest extends BaseTest {
+public class LoginTestWithDataBuilder {
 
-
-    @TmsLink("TEST-123")
     @Test(dataProvider = "loginCredData")
     public void testLogin(LoginCred loginCred) {
-        LoginFlow loginFlow = new LoginFlow(getDriver(), loginCred.getEmail(), loginCred.getPassword());
-        loginFlow.gotoLoginScreen();
-        loginFlow.login();
-        loginFlow.verifyLogin();
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.android);
+
+        try {
+            LoginFlow loginFlow = new LoginFlow(appiumDriver, loginCred.getEmail(), loginCred.getPassword());
+            loginFlow.gotoLoginScreen();
+            loginFlow.login();
+            loginFlow.verifyLogin();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        appiumDriver.quit();
     }
 
     @DataProvider
     public LoginCred[] loginCredData() {
-        String filePath = "/src/test/java/test_data/authen/LoginCreds.json";
+        String filePath = "/src/main/java/test_data/authen/LoginCreds.json";
         return DataObjectBuilder.buildDataObject(filePath, LoginCred[].class);
     }
 
